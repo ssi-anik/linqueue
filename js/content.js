6f0d3f9466097a65c4f982f652f6a2ebb75da022
me_shaon <shaon.cse81@gmail.com>
2016-09-10 21:24:38 +0600

    Unnecessary jQuery file removed and Finding clicked link text code updated

--- js/content.js
+++ js/content.js
@@ -1,17 +1,14 @@
-var title = "";
+var title = null;
 
-// get the link name from the context menu click
-// used delegate to retrive the values even after the dom loads
-$(document).on("contextmenu", "a", function(){ 
-	title = $(this).text().trim();
-});
+document.addEventListener("mousedown", function(event){
+    //catch the right click event
+    if(event.button == 2) { 
+        title = event.target.innerText;//get the clicked event text
+    }
+}, true);
 
-// broadcasted messages listener
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	// check the message
-	if(request.message === "linqueue_get_anchor_title"){
-		// return response
-		var response = {'title': title};
-		sendResponse(response);
-	}
+    if(request.message === "linqueue_get_anchor_title") {
+        sendResponse({value: title});
+    }
 });
